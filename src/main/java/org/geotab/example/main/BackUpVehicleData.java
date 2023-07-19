13cd4ea8ca0818441b5fefaf0ce90a9ce0e729bb
icanas <ivancanas@gmail.com>
2023-07-19 15:29:01 +0200

    clean

--- src/main/java/org/geotab/example/main/BackUpVehicleData.java
+++ src/main/java/org/geotab/example/main/BackUpVehicleData.java
@@ -7,8 +7,6 @@ import org.geotab.example.utils.ConnectionUtils;
 import org.geotab.example.utils.CsvUtils;
 
 import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class BackUpVehicleData {
 
@@ -18,31 +16,26 @@ public class BackUpVehicleData {
         LoginManager loginManager = new LoginManager();
         ConsoleController consoleController = new ConsoleController(loginManager);
 
-        // Create a Scanner object to read user input
         Scanner scanner = new Scanner(System.in);
 
-        // Display the options menu
         displayMenu();
 
-        // Read user input and execute corresponding option
         while (true) {
             System.out.print("Enter option (1: Execute, 2: Change CSV Path, 3: Print YML Values, 4: Change Execute Interval, 5: Exit): ");
             int option = scanner.nextInt();
-            scanner.nextLine(); // Consume the newline character
+            scanner.nextLine();
 
             switch (option) {
                 case 1:
                     consoleController.execute(EXECUTE_INTERVAL_MINUTES);
                     break;
                 case 2:
-                    // Change CSV data output path
                     System.out.print("Enter new CSV data output path: ");
                     String newPath = scanner.nextLine();
                     CsvUtils.setCsvResultPath(newPath);
                     System.out.println("CSV data output path changed to: " + newPath);
                     break;
                 case 3:
-                    // Print YAML file values (Assuming you have a method to read and display YAML values)
                     try {
                         printYamlFileValues("credentials.yml");
                     } catch (Exception e) {
@@ -50,14 +43,12 @@ public class BackUpVehicleData {
                     }
                     break;
                 case 4:
-                    // Change execute interval
                     System.out.print("Enter new execute interval (in minutes): ");
                     int newInterval = scanner.nextInt();
                     EXECUTE_INTERVAL_MINUTES = newInterval;
                     System.out.println("Execute interval changed to: " + newInterval + " minutes");
                     break;
                 case 5:
-                    // Stop the scheduled execution and exit the program
                     System.out.println("Exiting...");
                     System.exit(0);
                 default:
